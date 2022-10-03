146f24f5a9a3d9870b8b1858275d16099fbc4ba3
Chris Elias <chris.elias5@gmail.com>
2022-10-03 18:33:23 -0400

    ignore listening ports

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,25 @@ fn get_ephermeral_port_range() -> (i32, i32) {
     (low, high)
 }
 
+fn get_listening_ports_to_ignore() -> Vec<i32> {
+    let contents = fs::read_to_string("/proc/net/tcp").expect("Unable to read tcp file");
+    let mut ignore_ports: Vec<i32> = vec![];
+
+    let lines = contents.lines();
+
+    for line in lines {
+        if line.contains(":") {
+            let local_ip_ports: Vec<&str> = line.split_whitespace().collect();
+            let ports: Vec<&str> = local_ip_ports[1].split(':').collect();
+            let port = i32::from_str_radix(ports[1], 16).unwrap();
+
+            ignore_ports.push(port);
+        }
+    }
+
+    ignore_ports
+}
+
 fn main() {
     println!("Getting ephermeral port range");
     let (ephermeral_low, ephermeral_high) = get_ephermeral_port_range();
@@ -18,4 +37,7 @@ fn main() {
         "ephemeral port range: {:?}-{:?}",
         ephermeral_low, ephermeral_high
     );
+    let ignore_listeining_ports = get_listening_ports_to_ignore();
+
+    println!("listening ports to ignore: {:?}", ignore_listeining_ports);
 }
