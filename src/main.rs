581da8b78dbc02e121e7051fa5010bf5e3c19151
Chris Elias <chris.elias5@gmail.com>
2022-10-03 13:32:45 -0400

    get ephermeral port range

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::fs;
+
+fn get_ephermeral_port_range() -> (i32, i32) {
+    let contents = fs::read_to_string("/proc/sys/net/ipv4/ip_local_port_range")
+        .expect("Unable to read ip local port range file");
+
+    let port_range: Vec<&str> = contents.split_whitespace().collect();
+    let low: i32 = port_range[0].parse().unwrap();
+    let high: i32 = port_range[1].parse().unwrap();
+
+    (low, high)
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Getting ephermeral port range");
+    let (ephermeral_low, ephermeral_high) = get_ephermeral_port_range();
+    println!("Range, {:?}-{:?}", ephermeral_low, ephermeral_high);
 }
